ee02f05f974ed5183d6102737decfcb06afbfab6
gpelleran78 <geremy_pelleran@yahoo.com>
2020-01-13 22:16:03 -0700

    start of clicky game

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Card from './components/card';
 import './App.css';
-import FriendContainer from './components/friendcontainer/FriendContainer';
 
 function App() {
   return (
-       <FriendContainer />
+    <div>
+      <h1>Can You Remember Where the Cards Are?</h1>
+      <Card
+      id={1}
+      width={100}
+      height={100}
+      back={'/img/back.png'}
+      front={'/img/react.png'}
+      flipped={flipped.includes(1)}
+      handleClicked={() => handleClicked}
+      />
+    </div>
   );
 }
 
---
+++ clicky-game/src/components/card/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+export default function Card({ handleClicked, id, flipped, back, front, height, width }){
+    return
+    <div
+        className={'flip-container ${flipped ? 'flipped' : ''}'}  
+        style = {{ 
+            width,height
+        }} 
+        onclick={() => handleClick (id)}   
+        > 
+
+        <div className = "flipper">
+            <img 
+            style = {{
+                height,width
+            }}
+            className={flipped ? 'front' : 'back'}
+            src={flippped ? front : back}
+            />
+        </div>
+    </div>
+    )
+}
+
+Card.protoTypes = {
+    handleClicked: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+    flipped: PropTypes.bool.isRequired,
+    back: PropTypes.string.isRequired,
+    front: PropTypes.string.isRequired,
+    height: PropTypes.string .isRequired,
+    width: PropTypes.string.isRequired
+}
